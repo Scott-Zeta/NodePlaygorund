547dbc2324557f9b57efd8f0a8172af310a3ebf4
Scott Zeta <scott-zeta@outlook.com>
2024-07-05 16:18:17 +0930

    Implement TS abstract feature in common route config

--- RestApiTS/common/common.routes.config.ts
+++ RestApiTS/common/common.routes.config.ts
@@ -1,16 +1,20 @@
 // common route config for extends
 import express from 'express';
-export class CommonRoutesConfig {
+export abstract class CommonRoutesConfig {
   app: express.Application;
   name: string;
 
   constructor(app: express.Application, name: string) {
     this.app = app;
     this.name = name;
+    this.configureRoutes();
   }
   getName() {
     return this.name;
   }
+  abstract configureRoutes(): express.Application;
+  // Using Typescript abstract class to force the child class to implement the configureRoutes method.
+  // Any class extending CommonRoutesConfig must implement the configureRoutes methods matching the signature.
 }
 
 /* 
