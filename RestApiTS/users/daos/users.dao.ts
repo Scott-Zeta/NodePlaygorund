4a8715ccc1d0a6c568214bcb6938bb12dedb8c8c
Scott Zeta <scott-zeta@outlook.com>
2024-07-13 15:21:59 +0930

    Dao class and constructor

--- RestApiTS/users/daos/users.dao.ts
+++ RestApiTS/users/daos/users.dao.ts
@@ -6,3 +6,18 @@ import shortid from 'shortid';
 import debug from 'debug';
 
 const log: debug.IDebugger = debug('app:in-memory-dao');
+
+/*
+Using the singleton pattern, this class will always provide the same instance—and, 
+critically, the same users array—when we import it in other files.  
+*/
+
+class UsersDao {
+  users: Array<CreateUserDto> = [];
+
+  constructor() {
+    log('Created new instance of UsersDao');
+  }
+}
+
+export default new UsersDao();
