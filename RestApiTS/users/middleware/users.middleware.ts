e720263550f05301115799bc1ebbd140bb687e2e
Scott Zeta <scott-zeta@outlook.com>
2024-07-18 15:39:14 +0930

    fileds vlidation method

--- RestApiTS/users/middleware/users.middleware.ts
+++ RestApiTS/users/middleware/users.middleware.ts
@@ -3,6 +3,21 @@ import userService from '../services/users.service';
 import debug from 'debug';
 
 const log: debug.IDebugger = debug('app:users-controller');
-class UsersMiddleware {}
+class UsersMiddleware {
+  //validate requireed fields
+  async validateRequiredUserBodyFields(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.body && req.body.email && req.body.password) {
+      next();
+    } else {
+      res.status(400).send({
+        error: `Missing required fields email and password`,
+      });
+    }
+  }
+}
 
 export default new UsersMiddleware();
