73ee2a7cd9abd141aaca735524e9cf641fb3d7c3
Scott Zeta <scott-zeta@outlook.com>
2024-07-29 14:30:07 +0930

    add a help function for extract userID

--- RestApiTS/users/middleware/users.middleware.ts
+++ RestApiTS/users/middleware/users.middleware.ts
@@ -77,6 +77,15 @@ class UsersMiddleware {
       });
     }
   }
+
+  async extractUserId(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    req.body.id = req.params.userId;
+    next();
+  }
 }
 
 export default new UsersMiddleware();
