5ac61e8ebd608afe317c0da0d31bba42f25eb696
Scott Zeta <scott-zeta@outlook.com>
2024-08-02 16:18:20 +0930

    refactor sequence

--- RestApiTS/app.ts
+++ RestApiTS/app.ts
@@ -7,18 +7,18 @@ import cors from 'cors';
 import { CommonRoutesConfig } from './common/common.routes.config';
 import { UsersRoutes } from './users/users.routes.config';
 import debug from 'debug';
-
 import dotenv from 'dotenv';
-const dotenvResult = dotenv.config();
-if (dotenvResult.error) {
-  throw dotenvResult.error;
-}
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
 const routes: Array<CommonRoutesConfig> = [];
 const debugLog: debug.IDebugger = debug('app');
+
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  throw dotenvResult.error;
+}
 
 // here we are adding middleware to parse all incoming requests as JSON
 app.use(express.json());
