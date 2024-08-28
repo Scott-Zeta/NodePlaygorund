2572abd3e51590b5fbc3f61d800d5b1ef6c14036
Scott Zeta <scott-zeta@outlook.com>
2024-08-28 12:21:49 +0930

    feat: Add Prisma plugin and configure Prisma client
    This commit adds the Prisma plugin to the GraphQL server and configures the Prisma client. The Prisma plugin allows us to integrate Prisma into our GraphQL schema, and the Prisma client is used to interact with the database. This change enables us to use Prisma's powerful features for database operations.

--- GraphQL_FS/graphql-server/src/schema.ts
+++ GraphQL_FS/graphql-server/src/schema.ts
@@ -1,12 +1,16 @@
 import SchemaBuilder from '@pothos/core';
 import { DateResolver } from 'graphql-scalars';
+import PrismaPlugin from '@pothos/plugin-prisma';
+import type PrismaTypes from '@pothos/plugin-prisma/generated';
+import { prisma } from './db';
 
 //import Scalars Date type to be used in the Schemabuilder
 const builder = new SchemaBuilder<{
   Scalars: {
     Date: { Input: Date; Output: Date };
   };
-}>({});
+}>({ plugins: [PrismaPlugin], prisma: { client: prisma } });
+//builder require a prisma instance
 
 builder.addScalarType('Date', DateResolver, {});
 
